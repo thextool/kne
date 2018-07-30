365d2191c03f1a872d8faf2c5c94ada43b9f0f52
thextool <matthew.riach@gmail.com>
2018-07-30 15:08:18 -0600

    :zap: updated production env

--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection:'postgres://localhost:/knex-drill-1'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
