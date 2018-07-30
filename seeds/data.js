c1aeaf4a309680f8fdcdcbd5461731816a452546
thextool <matthew.riach@gmail.com>
2018-07-30 15:43:25 -0600

    :sparkles: seeded the resolutions db with the seed

--- seeds/data.js
+++ seeds/data.js
@@ -1,13 +1,23 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = async function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  await knex('resolutions').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex('resolutions').insert([{
+        id: 1, 
+        dueDate: '1997-02-01',
+        resolution: 'Go skiing'
+      },
+      {
+        id: 2, 
+        dueDate: '1997-05-01',
+        resolution: 'Do stand-up'
+      },
+      {
+        id: 3, 
+        dueDate: '1997-09-01',
+        resolution: 'Start knitting'
+      }]);
     });
 };
